88e52092171492883607b3a6f04de78b30ab5176
nqtinh <nqtinh97@gmail.com>
2018-12-03 22:58:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import DefaultLayout from './components/DefaultLayout.js';
 import Login from './components/User/login';
@@ -8,13 +8,13 @@ import Register from './components/User/register';
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+      <Router>
         <Switch>
           <Route path="/login" name="Login" component={Login} />
           <Route path="/register" name="Register" component={Register} />
           <Route path="/" name="Home" component={DefaultLayout} />
         </Switch>
-      </HashRouter>
+      </Router>
     ); 
   }
 }
