5c5eb6c1c5c9b319c6220b6911cdbecbc49ea926
kennySix <kraken.kid196@gmail.com>
2018-12-04 13:49:38 +0700

    new update reducer

--- src/reducers/FollowList/followersReducer.js
+++ src/reducers/FollowList/followersReducer.js
@@ -5,16 +5,22 @@ const initialState = {
             name: "Sophia Lee",
             carrer:"Student at Harvard",
             friendOrNot: true,
+            avatar:"http://support.overwolf.com/wp-content/uploads/2016/07/avatar-300x300.png",
+            cover:"https://4.bp.blogspot.com/-B0fG_8swDak/Wa95uQ2imlI/AAAAAAAAAZY/xB-50xkECvw-F5dkhgX4DvDb4YvYwI4dACLcBGAs/s1600/cover-workshop-after-effect-phongluong-1030x360.jpg",
         },
         {
             name: "John Dee",
             carrer:"Traveller",
             friendOrNot: true,
+            avatar:"https://avatarfiles.alphacoders.com/792/79295.jpg",
+            cover:"http://danceabilityinc.com/wp-content/uploads/2018/09/halloween-1030x360.jpg",
         },
         {
             name: "Julia Cox",
             carrer:"Art Designer",
             friendOrNot: false,
+            avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/8f024bc0-1def-4a70-86c6-69360a2280b9-profile_image-300x300.png",
+            cover:"https://www.scarsdalevets.com/wp-content/uploads/2017/03/Gastrope-Equine-Cover-Photo-1-1030x360.jpg",
         }
     ]
 };
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import {combineReducers} from 'redux';
 import followingReducer from './FollowList/followingReducer';
 import {followersReducer} from './FollowList/followersReducer';
 import {personalReducer} from './PersonalPage/personalReducer';
+import {personInfoReducer} from './PersonalPage/personInfoReducer';
 const rootReducer = combineReducers({
     loginReducer,
     registerReducer,
     followingReducer,
     followersReducer,
     personalReducer,
+    personInfoReducer
 });
 export default rootReducer;
