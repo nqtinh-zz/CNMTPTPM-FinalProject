b8c6c98f1cf4d65e2e18455959a085dcfbaa9d67
nqtinh <37231358+nqtinh@users.noreply.github.com>
2018-12-26 23:51:56 +0700

    optimize speed

--- back-end/router/server/block.js
+++ back-end/router/server/block.js
@@ -1,5 +1,7 @@
 var axios = require('axios');
 var block = require('../../models/blocks');
+var alltx = require('../../models/alltx');
+var { encode, decode, decodeFollowings, ReactContent, PlainTextContent } = require('../../lib/transaction/v1');
 const getData1 = async (i) => {
     try {
         return await axios.get('https://komodo.forest.network/block?height=' + i)
@@ -7,6 +9,7 @@ const getData1 = async (i) => {
         //console.log("loi roi")
     }
 }
+//tối ưu hóa chỗ tạo ra các thay đổi user để tránh duyệt từ đầu đến cuối bảng users
 exports.saveBlock = async () => {
     const response = await axios.get('https://fox.forest.network/abci_info');
     let curheight = response.data.result.response.last_block_height;
@@ -14,27 +17,61 @@ exports.saveBlock = async () => {
     const data = await block.find({}).sort({ height: -1 });
     let i = data[0].height + 1;
     console.log(i);
+    await block.findOneAndUpdate({ height: data[0].height },
+        {
+            $set: {
+
+                lastheightposi: data[0].height,
+            }
+        })
     let arr = [];
     for (i; i <= curheight; i++) {
         arr.push(new Promise(async (resolve) => {
             const blocks = await getData1(i);
+
+            console.log(i+'----'+blocks);
             if (blocks != undefined) {
-                console.log(i);
+                console.log('i----' + i)
                 const blockData = blocks.data.result;
-                const block = new Block({
-                    height: blockData.block.header.height,
-                    time: blockData.block.header.time,
-                    txs: blockData.block.data.txs,
-                    hash: blockData.block_meta.block_id.hash,
-                    appHash: blockData.block.header.app_hash,
-                })
-                if (blockData.block.data.txs != null) {
-                    try {
-                        const a = await block.save();
-                    } catch (error) {
-                        //console.log(error);
+                let tmptxs = blockData.block.data.txs;
+                if (tmptxs != null) {
+                    for (let n = 0; n < tmptxs.length; n++) {
+                        let tmpaction = [];
+                        let decoded = decode(Buffer.from(tmptxs[n], 'base64'));
+                        if (decoded.operation == 'interact') {
+                            tmpaction.push(decoded.account);
+                            await alltx.find({ hash: decoded.params.object }).then((arr) => {
+                                for (let j = 0; j < arr.length; j++) {
+                                    tmpaction.push(arr[j].publicKey);
+                                }
+                            });
+                        }
+                        if (decoded.operation == 'payment') {
+                            tmpaction.push(decoded.account);
+                            tmpaction.push(decoded.params.address);
+                        }
+                        if (decoded.operation == 'create_account') {
+                            tmpaction.push(decoded.params.address);
+                            tmpaction.push(decoded.account);
+                        }
+                        else {
+                            tmpaction.push(decoded.account);
+                        }
+                        const block = new Block({
+                            height: blockData.block.header.height,
+                            time: blockData.block.header.time,
+                            txs: blockData.block.data.txs,
+                            hash: blockData.block_meta.block_id.hash,
+                            appHash: blockData.block.header.app_hash,
+                            newActionUser: tmpaction,
+
+                        })
+
+                        await block.save();
+
                     }
                 }
+
             }
             else { i = i - 1 }
             resolve("test");
